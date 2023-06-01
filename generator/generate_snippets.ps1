f4f153d5d75dc6a788357862216faad865a2a9e5
Dominykas M <26067386+Tasty-Kiwi@users.noreply.github.com>
2023-06-01 20:16:04 +0300

    update homepage and make snippets ordered to make diffs smaller in the future

--- generator/generate_snippets.ps1
+++ generator/generate_snippets.ps1
@@ -7,21 +7,21 @@ $raw_docs = (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/pewpewliv
 # Strip `var documentation = ` to make it compatible with JSON
 $raw_docs = $raw_docs.Replace("var documentation = ", "")
 
-$docs_obj = ConvertFrom-Json $raw_docs
+$docs_obj = ConvertFrom-Json $raw_docs -AsHashtable
 
 $pewpew = $docs_obj[0]
 $fmath = $docs_obj[1]
 
-function Generate-Enums {
+function Out-Enums {
   param (
     $table_name,
     $enums
   )
   
-  $enums_obj = @{}
+  $enums_obj = [ordered]@{}
   foreach ($enum in $enums) {
     foreach ($value in $enum.values) {
-      $enums_obj[("{0}_{1}" -f $enum.name, $value)] = @{}
+      $enums_obj[("{0}_{1}" -f $enum.name, $value)] = [ordered]@{}
       $enums_obj[("{0}_{1}" -f $enum.name, $value)]["prefix"] = ("{0}.{1}.{2}" -f $table_name, $enum.name, $value)
       $enums_obj[("{0}_{1}" -f $enum.name, $value)]["body"] = ("{0}.{1}.{2}" -f $table_name, $enum.name, $value)
     }
@@ -30,17 +30,17 @@ function Generate-Enums {
   return $enums_obj
 }
 
-function Generate-Functions {
+function Out-Functions {
   param (
     $table_name,
     $funcs
   )
   
-  $functions_obj = @{}
+  $functions_obj = [ordered]@{}
 
   foreach ($func in $funcs) {
     $func_args = ""
-    $functions_obj[$func.func_name] = @{}
+    $functions_obj[$func.func_name] = [ordered]@{}
     $functions_obj[$func.func_name]["prefix"] = ("{0}.{1}" -f $table_name, $func.func_name)
     for ($i = 0; $i -lt $func.parameters.Count; $i++) {
       if ($i -eq $func.parameters.Count - 1) {
@@ -56,9 +56,9 @@ function Generate-Functions {
   return $functions_obj
 }
 
-$pewpew_enums = ConvertTo-Json (Generate-Enums $pewpew.name $pewpew.enums)
-$pewpew_functions = ConvertTo-Json (Generate-Functions $pewpew.name $pewpew.functions)
-$fmath_functions = ConvertTo-Json (Generate-Functions $fmath.name $fmath.functions)
+$pewpew_enums = ConvertTo-Json (Out-Enums $pewpew.name $pewpew.enums)
+$pewpew_functions = ConvertTo-Json (Out-Functions $pewpew.name $pewpew.functions)
+$fmath_functions = ConvertTo-Json (Out-Functions $fmath.name $fmath.functions)
 
 "// Auto-generated PewPew enums from ppl-utils (https://github.com/pewpewlive/ppl-utils)`n$pewpew_enums" | Out-File -Encoding utf8 -FilePath "./snippets/generated/pewpew-enums.code-snippets"
 "// Auto-generated PewPew funcs from ppl-utils (https://github.com/pewpewlive/ppl-utils)`n$pewpew_functions" | Out-File -Encoding utf8 -FilePath "./snippets/generated/pewpew-funcs.code-snippets"
